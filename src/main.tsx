a32a6e3b7196dc8d73c3b3085f9eb6777b5ede98
pourzarrabi <pourzarrabi@gmail.com>
2023-10-01 17:46:05 +0330

    Implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
 import theme from './theme.ts';
